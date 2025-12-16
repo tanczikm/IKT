307a07adfd1dd99d7b32063dda3256e7fa5c1602
tanczikm <tanczikm@kkszki.hu>
2025-12-16 14:08:11 +0100

    2025. 12. 16.

--- python/bmi.py
+++ python/bmi.py
@@ -4,5 +4,19 @@ magassag = float(input("Add meg a magasságod méterben! "))
 
 bmi = (testsuly / (magassag * magassag))
 bmi = round(bmi, 2)
-print("A BMI értéked:", bmi)
+print(f"A BMI értéked: {bmi}")
 
+# Függvény hívásánál
+print(f"Eredmény: {round(25.48475, 1)}") # Eredmény: 25,5
+
+#Egy tanuló adatainak használata
+
+tanulo = {
+    "nev": "Péter",
+    "kor": 42,
+    "magassag": "175"
+}
+
+# Helyörző használata
+
+print("BMI értéked: {}".format(23.15))
