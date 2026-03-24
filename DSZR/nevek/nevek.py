1132bf9676b28f0fe7828fa7ef0b5813e2924e4c
tanczikmate <70233127+tanczikmate@users.noreply.github.com>
2026-03-24 22:20:45 +0100

    DSZR - "Nevek" kezdetleges feladatjavaslat

---
+++ DSZR/nevek/nevek.py
@@ -0,0 +1,14 @@
+# a legnepszerubb_nevek.txt állományban a 2023-ban és 2024-ben legnépszerűbb fiú és lánynevek vannak pontosvesszővel elválasztva.
+# az 1. sorban a 2023-ban legnépszerűbb fiú,
+# a 2. sorban a 2023-ban legnépszerűbb lány,
+# a 3. sorban a 2024-ben legnépszerűbb fiú,
+# a 4. sorban a 2024-ben legnépszerűbb lány nevekkel
+
+# kiszámítani pl:
+# hány olyan fiú és lány név van ami nem található meg mindkét éven a legnépszerűbb nevek között
+# átlagosan hány betű hosszúak a lány és a fiú nevek
+# kiírni a top5 fiú és lány nevet mind a két évre
+
+# név lekérdező szolgáltatás:
+# bekérni a nevet
+# kiírni hogy melyik évben hanyadik legnépszerűbb név volt, ha valamelyik éven nem szerepel a top nevek között akkor kiírni azt hogy nincs benne az 50-ben
